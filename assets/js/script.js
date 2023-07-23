1aff3c3ba0f878ad2887201458f821d8b0a06ba7
unknown <blancardt@merrimack.edu>
2023-07-23 11:29:39 -0400

    made the start quiz button disapear when clicked on.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Selects element by class
 var countDown = document.querySelector("#time-remaining");
 var startButton = document.querySelector("#start");
+var multipleChoiceList = document.querySelector("#options");
 
 var secondsRemaining = 0;
 
@@ -22,8 +23,15 @@ function setTime() {
 }
 
 function startQuiz() {
+    //start the timer.
     secondsRemaining = 75;
     setTime();
+
+    //hide the start quiz button.
+    startButton.setAttribute("style", "display: none");
+
+    //call a function that adds the multiple choice options to the page along with changing the header to the questions.
+
 }
 
 startButton.addEventListener("click", startQuiz);
