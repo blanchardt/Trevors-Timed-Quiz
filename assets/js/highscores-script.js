e1d2eb96232c5f3545e4104b9241a76775c032b4
unknown <blancardt@merrimack.edu>
2023-07-25 14:53:04 -0400

    created a new js file, that populates the unordered list for the new html file and added some styling for the new file.

---
+++ assets/js/highscores-script.js
@@ -0,0 +1,25 @@
+// create variables getting commonly used elements.
+var mainEl = document.querySelector("main");
+var ulEl = document.querySelector("#high-scores");
+
+//create some global variables.
+var results = [];
+
+//arange the text to left instead of center.
+mainEl.setAttribute("style", "text-align: left");
+
+//get the stored scores and then add them to the unordered list.
+var storedScores = JSON.parse(localStorage.getItem("scores"));
+if (storedScores !== null) {
+  results = storedScores;
+}
+
+//generate the list.
+for (var i = 0; i < results.length; i++) {
+  var result = results[i];
+
+  var li = document.createElement("li");
+  li.textContent = (i + 1) + ". " + result.initials + " - " + result.score;
+  
+  ulEl.appendChild(li);
+}
